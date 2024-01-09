27634a1ed634d9df5a930fe3f2ee6c8834f3408a
Madi Karsybekov <madikarsybekov@gmail.com>
2024-01-09 21:38:21 +0100

    Kahn Process Network done

--- kpn.cpp
+++ kpn.cpp
@@ -4,3 +4,26 @@
 #include "kpn.h"
 
 // ADD THINGS HERE
+void kpn::Add() {
+    while(true) {
+        b.write(a.read() + c.read());
+    }
+}
+
+void kpn::Delay() {
+    while(true) {
+        c.write(d.read());
+    }
+}
+
+void kpn::Split() {
+    int i = 0;
+    while(i < 10) {
+        unsigned int temp = b.read();
+        a.write(temp);
+        d.write(temp);
+        std::cout << "The output e is: " << temp << std::endl; // Fibonacci numbers
+        i++;
+    }
+    sc_stop();
+}
--- kpn.h
+++ kpn.h
@@ -7,11 +7,20 @@ SC_MODULE(kpn)
 {
 private:
     // ADD THINGS HERE
+    sc_fifo<unsigned int> a, b, c, d;
+    void Add();
+    void Split();
+    void Delay();
 
 public:
-    SC_CTOR(kpn) // : ADD THINGS HERE
+    SC_CTOR(kpn) : a(10), b(10), c(10), d(10) // : ADD THINGS HERE
     {
         // ADD THINGS HERE
+        b.write(1);
+        c.write(0);
+        SC_THREAD(Split);
+        SC_THREAD(Add);
+        SC_THREAD(Delay);
     }
 };
 
